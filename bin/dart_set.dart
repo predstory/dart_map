4bcb4e4641286f74df45379f40c3571cb6e0847b
predstory <predstory@yandex.ru>
2023-12-23 12:41:31 +0300

    rename

--- bin/dart_set.dart
+++ bin/dart_set.dart
@@ -10,11 +10,12 @@ void main() {
   aleksandr["zp"] = 100000; //таким образом можно добавить в сет пару
   //ключ значение
 
-  aleksandr["male"] = "female";
+  aleksandr.addAll({"language": "Russia", "mobile": "iPhone 13 Pro"}); //
 
   print(aleksandr.keys); //возвращает ключи
   print(aleksandr.values); //возвращает значения
   print(aleksandr.length); //возвращает ответ сколько связок ключ : значение
   print(aleksandr.isEmpty); //там пусто?
   print(aleksandr.isNotEmpty); //там не пусто, да?
+  print(aleksandr);
 }
