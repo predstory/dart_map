03c2642f70659e5c79a551c74dc02f3e9f589cbd
predstory <predstory@yandex.ru>
2023-12-23 12:26:55 +0300

    set dart

---
+++ bin/dart_set.dart
@@ -0,0 +1,5 @@
+import 'package:dart_set/dart_set.dart' as dart_set;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_set.calculate()}!');
+}
---
+++ test/dart_set_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_set/dart_set.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
